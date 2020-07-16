88ed2e90572c2e22a7114cfaebb1d63d5eec2e18
Simon <simibac93@gmail.com>
2020-07-16 15:58:08 +0200

    function to display buying and selling queues

--- index.js
+++ index.js
@@ -25,3 +25,44 @@ exports.fungibleBaseId = new BigNumber(
 exports.nonFungibleBaseId = new BigNumber(
   "57896044618658097711785492504343953926975274699741220483192166611388333031424"
 );
+
+exports.printQueues = async (event, type) => {
+  const granularity = await event.granularity();
+  for (i = 1; i <= granularity.toNumber(); i++) {
+    let percentage = (100 / granularity.toNumber()) * i;
+
+    let queue = await event.buyingQueue(type, percentage);
+
+    let head = queue["head"];
+    let tail = queue["tail"];
+    let numberTickets = queue["numberTickets"];
+    let queueString = (percentage + "% (" + numberTickets + ")").padEnd(12);
+
+    // buying queue
+    let buyingString = "";
+    for (j = tail - 1; j >= head; --j) {
+      let queueEntryBuying = await event.getQueuedUserBuying(
+        type,
+        percentage,
+        j
+      );
+      buyingString += queueEntryBuying["quantity"];
+    }
+    queueString += buyingString.padStart(15) + " --- ";
+
+    //selling queue
+    let sellingString = "";
+    for (j = head; j < head; j++) {
+      let queueEntrySelling = await event.getQueuedUserBuying(
+        type,
+        percentage,
+        j
+      );
+      sellingString += queueEntrySelling["quantity"];
+    }
+
+    queueString += sellingString;
+
+    console.log(queueString);
+  }
+};
