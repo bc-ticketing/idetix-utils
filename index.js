282e15bc9a41fb93431d18dac4b261feb4e808a3
Simon <simibac93@gmail.com>
2020-07-23 11:34:05 +0200

    pretty print for nf selling orders

--- index.js
+++ index.js
@@ -17,14 +17,12 @@ exports.argsToCid = (hashFunction, size, digest) => {
 };
 
 // number is is equivalent to 1(128*0)
-exports.fungibleBaseId = new BigNumber(
-  "340282366920938463463374607431768211456"
-);
+const fungibleBaseId = new BigNumber("340282366920938463463374607431768211456");
+exports.fungibleBaseId;
 
 // which is equivalent in binary to 1(126*0)1(128*0)
-exports.nonFungibleBaseId = new BigNumber(
-  "57896044618658097711785492504343953926975274699741220483192166611388333031424"
-);
+const nonFungibleBaseId = new BigNumber("57896044618658097711785492504343953926975274699741220483192166611388333031424");
+exports.nonFungibleBaseId;
 
 exports.prettyPrintAddress = (address) => address.slice(0, 6) + "..." + address.slice(-4);
 
@@ -91,7 +89,7 @@ exports.printNfSellOrders = async(event, type) => {
     "Percentage".padStart(percentageFormatLength)
   );
 
-  for(i=nonFungibleBaseId.plus(1, 10); i.isLessThan(nonFungibleBaseId.plus(supply, 10)); i=i.plus(1, 10)){
+  for(i=type.plus(1, 10); i.isLessThan(type.plus(supply, 10)); i=i.plus(1, 10)){
     let sellOrder = await event.nfTickets(i.toFixed());
     let seller = new BigNumber(sellOrder["userAddress"])
     if(!seller.isZero()){
